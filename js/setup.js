565071090c351570dc24dc40f0588d0d1263373c
edumt <edumoura1997@gmail.com>
2021-08-06 19:26:06 -0300

    adjusted carousel responsiveness + cleared css

--- js/setup.js
+++ js/setup.js
@@ -2,15 +2,19 @@ $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:false,
+    stagePadding:30,
     responsive:{
         0:{
-            items:1
+            items:2
         },
         600:{
-            items:3
+            items:4
         },
         1000:{
-            items:5
+            items:6
+        },
+        1500:{
+            items:8
         }
     }
 })
