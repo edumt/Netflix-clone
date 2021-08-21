b48dc2da306edb3442f99d86c6a7c0abc4d94b3e
edumt <edumoura1997@gmail.com>
2021-08-21 19:48:25 -0300

    fixed profile imgs flickering on hover

--- js/setup.js
+++ js/setup.js
@@ -1,22 +1,25 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    dots:false,
-    lazyLoad:true,
-    stagePadding:30,
-    responsive:{
-        0:{
-            items:2
-        },
-        600:{
-            items:4
-        },
-        1000:{
-            items:6
-        },
-        1400:{
-            items:8
-        }
-    }
-})
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  dots: false,
+  lazyLoad: true,
+  stagePadding: 30,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    400: {
+      items: 3,
+    },
+    600: {
+      items: 4,
+    },
+    1000: {
+      items: 6,
+    },
+    1400: {
+      items: 8,
+    },
+  },
+});
